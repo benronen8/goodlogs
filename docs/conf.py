d6bf15196075085eb35818e29db2b542c58cc410
benronen8 <benronen8@gmail.com>
2025-12-17 20:48:23 +0200

    add cicd with sphinx docs generation

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# Configuration file for the Sphinx documentation builder.
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+project = 'goodlog'
+copyright = '2025, Ben Ronen'
+author = 'Ben Ronen'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
