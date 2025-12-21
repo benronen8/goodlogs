9dc12db76f3684595db5e751f6156663c2dfeddc
benronen8 <benronen8@gmail.com>
2025-12-21 09:55:38 +0200

    up version

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "goodlog"
 copyright = "2025, Ben Ronen"
 author = "Ben Ronen"
-release = "0.0.1"
+release = "0.0.1-rc2"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
